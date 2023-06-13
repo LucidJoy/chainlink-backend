38946019b320b272a1bb9d9ddc134fdea2f1dea9
Jyotirmoy <jyotirmoykarmakar02@gmail.com>
2023-06-14 01:42:54 +0530

    debug

--- index.js
+++ index.js
@@ -3,63 +3,123 @@ const app = express();
 const PORT = 3000;
 
 app.get("/graph-rnn-1", (req, res) => {
-  res.send("55.60");
+  const response = {
+    message: 55.6,
+  };
+
+  res.json(response);
 });
 
 app.get("/graph-rnn-2", (req, res) => {
-  res.send("50.24");
+  const response = {
+    message: 50.24,
+  };
+
+  res.json(response);
 });
 
 app.get("/graph-rnn-3", (req, res) => {
-  res.send("40.78");
+  const response = {
+    message: 40.78,
+  };
+
+  res.json(response);
 });
 
 app.get("/graph-rnn-4", (req, res) => {
-  res.send("43.67");
+  const response = {
+    message: 43.67,
+  };
+
+  res.json(response);
 });
 
 app.get("/graph-rnn-5", (req, res) => {
-  res.send("52.98");
+  const response = {
+    message: 52.98,
+  };
+
+  res.json(response);
 });
 
 app.get("/graph-rnn-6", (req, res) => {
-  res.send("53.52");
+  const response = {
+    message: 53.52,
+  };
+
+  res.json(response);
 });
 
 app.get("/graph-rnn-7", (req, res) => {
-  res.send("56.04");
+  const response = {
+    message: 56.04,
+  };
+
+  res.json(response);
 });
 
 app.get("/graph-rnn-8", (req, res) => {
-  res.send("58.76");
+  const response = {
+    message: 58.76,
+  };
+
+  res.json(response);
 });
 
 app.get("/graph-rnn-9", (req, res) => {
-  res.send("55.13");
+  const response = {
+    message: 55.13,
+  };
+
+  res.json(response);
 });
 
 app.get("/graph-rnn-10", (req, res) => {
-  res.send("53.87");
+  const response = {
+    message: 53.87,
+  };
+
+  res.json(response);
 });
 
 app.get("/graph-rnn-11", (req, res) => {
-  res.send("54.17");
+  const response = {
+    message: 54.17,
+  };
+
+  res.json(response);
 });
 
 app.get("/graph-rnn-12", (req, res) => {
-  res.send("52.63");
+  const response = {
+    message: 52.63,
+  };
+
+  res.json(response);
 });
 
 app.get("/graph-rnn-13", (req, res) => {
-  res.send("56.67");
+  const response = {
+    message: 56.67,
+  };
+
+  res.json(response);
 });
 
 app.get("/graph-rnn-14", (req, res) => {
-  res.send("55.04");
+  const response = {
+    message: 55.04,
+  };
+
+  res.json(response);
 });
 
 app.get("/graph-rnn-15", (req, res) => {
-  res.send("54.97");
+  const response = {
+    message: 54.97,
+  };
+
+  res.json(response);
 });
 
 app.listen(PORT, () => {
